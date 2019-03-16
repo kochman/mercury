60459b002c743a79f17723768fa596c9f14f49c0
Andrew Qu <qua@rpi.edu>
2019-03-16 17:49:53 -0400

    self api data point is fine

--- api.go
+++ api.go
@@ -7,6 +7,12 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+
+	"encoding/pem"
+
+	"os"
+
+	log "github.com/sirupsen/logrus"
 )
 
 
@@ -20,10 +26,26 @@ func CreateRoutes(store *Store){
 	r.Use(middleware.DefaultCompress)
 
 	r.Get("/self", func(w http.ResponseWriter, r *http.Request){
-		fmt.Print(r)
-		w.Write([]byte(i.Name))
+		w.Header().Set("Content-Type", "application/json")
+
+
+		//create the pem object to perform encoding
+		block := &pem.Block{
+			Type: "MESSAGE",
+			Bytes: []byte(i.PublicKey),
+		}
+	
+		// writes human readable public key to page
+		w.Write(pem.EncodeToMemory(block))
+		fmt.Println()
+	})
+	
+	//parse through messages that pertain to certain user
+	r.Get("/messages", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("your messages go here"))
 	})
 
+	// listen
 	http.ListenAndServe(":3000", r)
 
 }

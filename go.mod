341084da5abc6c937abf1f94150cf7c12176058f
Sidney Kochman <sidney@kochman.org>
2019-03-16 16:57:54 -0400

    zeroconf

--- go.mod
+++ go.mod
@@ -3,8 +3,20 @@ module github.com/kochman/mercury
 go 1.11
 
 require (
+	github.com/DataDog/zstd v1.3.5 // indirect
+	github.com/Sereal/Sereal v0.0.0-20190203221631-e99c7e10fca4 // indirect
 	github.com/asdine/storm v2.1.2+incompatible
+	github.com/cenkalti/backoff v2.1.1+incompatible // indirect
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/grandcat/zeroconf v0.0.0-20190118114326-c2d1b4121200
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/miekg/dns v1.1.6 // indirect
 	github.com/sirupsen/logrus v1.4.0
-	github.com/tyler-smith/go-bip39 v1.0.0
+	github.com/vmihailenco/msgpack v4.0.2+incompatible // indirect
 	go.etcd.io/bbolt v1.3.2 // indirect
+	golang.org/x/net v0.0.0-20190313220215-9f648a60d977 // indirect
+	golang.org/x/sync v0.0.0-20190227155943-e225da77a7e6 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- peer_manager.go
+++ peer_manager.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+
 	log "github.com/sirupsen/logrus"
+	"github.com/grandcat/zeroconf"
 )
+
+const MulticastGroupAddr = "[ff12::9316]:9316"
 
 type PeerManager struct{}
 
@@ -13,5 +18,98 @@ func NewPeerManager() *PeerManager {
 func (pm *PeerManager) Run() {
 	log.Debug("PeerManager run")
 
-	// join multicast group, send broadcast messages, handle incoming messages...
+	// register us
+	server, err := zeroconf.Register("Mercury", "_mercury._tcp", "local.", 9316, nil, nil)
+	if err != nil {
+		log.WithError(err).Error("unable to register zeroconf service")
+		return
+	}
+	defer server.Shutdown()
+
+	// find others
+	entries := make(chan *zeroconf.ServiceEntry)
+	resolver, err := zeroconf.NewResolver(nil)
+	if err != nil {
+		log.WithError(err).Error("unable to create resolver")
+		return
+	}
+	ctx := context.Background()
+	err = resolver.Browse(ctx, "_mercury._tcp", "local.", entries)
+	if err != nil {
+		log.WithError(err).Error("unable to browse")
+		return
+	}
+
+	for entry := range entries {
+		log.Debug(entry)
+	}
 }
+
+// func oldpeermanagerstuff() {
+// 	// join multicast group, send broadcast messages, handle incoming messages...
+// 	groupAddr, err := net.ResolveUDPAddr("udp6", MulticastGroupAddr)
+// 	if err != nil {
+// 		log.WithError(err).Error("unable to resolve multicast group addr")
+// 		return
+// 	}
+
+// 	c, err := net.ListenPacket("udp6", ":0")
+// 	if err != nil {
+// 		log.WithError(err).Error("unable to announce")
+// 		return
+// 	}
+// 	defer c.Close()
+
+// 	intfs, err := net.Interfaces()
+// 	if err != nil {
+// 		log.WithError(err).Error("unable to get interfaces")
+// 		return
+// 	}
+
+// 	p := ipv6.NewPacketConn(c)
+
+// 	joined := false
+// 	for _, intf := range intfs {
+// 		if err := p.JoinGroup(&intf, groupAddr); err != nil {
+// 			continue
+// 		}
+// 		joined = true
+// 	}
+// 	if !joined {
+// 		log.Error("unable to join multicast group on any interface")
+// 		return
+// 	}
+
+// 	go func() {
+// 		wcm := &ipv6.ControlMessage{
+// 			HopLimit: 1,
+// 		}
+// 		ticker := time.Tick(time.Second)
+// 		for range ticker {
+// 			for _, intf := range intfs {
+// 				wcm.IfIndex = intf.Index
+// 				_, err := p.WriteTo([]byte("hello world"), wcm, groupAddr)
+// 				if err != nil {
+// 					// this will fail on a lot of interfaces
+// 					continue
+// 				}
+// 				log.Debugf("wrote announcement to group on %s", intf.Name)
+// 			}
+// 		}
+// 	}()
+
+// 	b := make([]byte, 1500)
+// 	for {
+// 		n, _, src, err := p.ReadFrom(b)
+// 		if err != nil {
+// 			log.WithError(err).Error("unable to read")
+// 			continue
+// 		}
+// 		// if rcm.Dst.IsMulticast() {
+// 		// 	if rcm.Dst.Equal(groupAddr) {
+// 		// 		log.Debug("message to group")
+// 		// 	}
+// 		// }
+// 		log.Debugf("got %d-byte message from %s: \"%s\"", n, src.String(), b)
+// 	}
+// }

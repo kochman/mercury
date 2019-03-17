fca50b074ea6757b844c77ef6ac8b5c0fe531da2
Andrew Qu <qua@rpi.edu>
2019-03-16 21:25:32 -0400

    initial backend:

--- static/home/home.js
+++ static/home/home.js
@@ -3,7 +3,8 @@
 function validateForm(){
 	let unique_id = document.getElementById("unique_id").value;	
 	let message = document.getElementById("message").value;
-	if (message.length == 0){
+	if (message.length == 0) {
+		alert("invalid message")
 		return false;
 	}
 	sendMessage();
@@ -17,8 +18,8 @@ function sendMessage() {
 		url: "localhost:3000",
 		method: "POST",
 		data: {
-			TargetUserID: $("#unique_id"),
-			Message: $("#message")
+			TargetUserID: $("#unique_id").val(),
+			Message: $("#message").val()
 		},
 		success: function (result) {
 			console.log("success")

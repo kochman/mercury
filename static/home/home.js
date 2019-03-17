045bcdce5715cd29af55fbd6c338e8342fdbd218
Joseph Om <joeom516@gmail.com>
2019-03-16 23:18:57 -0400

    hello

--- static/home/home.js
+++ static/home/home.js
@@ -31,11 +31,28 @@ $(document).ready(function () {
 	fetch("/api/contacts/all").then(function (response) {
 		return response.json();
 	}).then(function (data) {
-		console.log(data);
+		//console.log(data);
 		for (var i = 0; i < data.length; i++){
 			$("#unique_id").append("<option value=" + data[i].Name + ">"+data[i].Name +"</option>");
 		}
 	})
+
+	fetch("/api/self").then(function (response) {	
+		return response.text();
+	}).then(function (data) {
+		console.log(data);
+		let result = "";
+		let count = 0;
+		let i = 26;
+		while (count <= 10){
+			result += data[i];
+			i++;
+			count++;
+		}
+		result += "...";
+		$("#personal-id").text(result);
+	})
 })
+
 
 

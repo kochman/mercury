fcf2e3915918acebd14695434f1d7da2079158b2
Andrew Qu <qua@rpi.edu>
2019-03-16 21:28:26 -0400

    resolved merge conflicts

---
+++ frontend/home/home.js
@@ -0,0 +1,31 @@
+
+// Function to validate that the message form is not blank 
+function validateForm(){
+	let unique_id = document.getElementById("unique_id").value;	
+	let message = document.getElementById("message").value;
+	if (message.length == 0){
+		return false;
+	}
+	sendMessage();
+	return true;
+}
+
+// Function to send the message 
+function sendMessage() {
+	
+	$.ajax({
+		url: "localhost:3000/send",
+		method: "POST",
+		data: {
+			TargetUserID: $("#unique_id"),
+			Message: $("#message")
+		},
+		success: function (result) {
+			console.log("success")
+			console.log(result)
+		}
+	})
+	return false;
+}
+
+
